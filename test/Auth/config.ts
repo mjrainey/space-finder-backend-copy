3b8fc728fa3446253559d8f4c053e2b705d007db
mjrainey <mjrainey@mindspring.com>
2021-09-12 10:39:43 -0400

    Updated config properties for AWS

--- test/Auth/config.ts
+++ test/Auth/config.ts
@@ -1,10 +1,10 @@
 
 
 export const config = {
-    REGION: 'eu-west-1',
-    USER_POOL_ID: 'eu-west-1_gRIUoOdLx',
-    APP_CLIENT_ID: '335vma5jmludo42mn2895cu5bh',
-    IDENTITY_POOL_ID: 'eu-west-1:c1490135-4dd2-4a82-9959-880f77fef599',
-    TEST_USER_NAME: 'barosanu2',
-    TEST_USER_PASSWORD: 'askjskfT7sdf&'
+    REGION: 'us-east-2',
+    USER_POOL_ID: 'us-east-2_1tSiQqEne',
+    APP_CLIENT_ID: '6d2dkqosknnb0paop7qsk50cnk',
+    IDENTITY_POOL_ID: 'us-east-2:b3a23405-e079-4b98-9be1-cee2f8cf5e82',
+    TEST_USER_NAME: 'mjrainey',
+    TEST_USER_PASSWORD: '7#oLiU&623H@toOSfeia'
 }
